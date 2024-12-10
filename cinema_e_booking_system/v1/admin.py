ebdd737a98cae402fd259293d0e3a847620edc85
Pavan Indukuri <pavanindukuri@Pavans-MacBook-Pro-2.local>
2024-12-10 13:59:09 -0500

    singletonchanges

--- cinema_e_booking_system/v1/admin.py
+++ cinema_e_booking_system/v1/admin.py
@@ -5,3 +5,11 @@ from .models import Booking, Promotion, PaymentCard
 admin.site.register(Booking)
 admin.site.register(Promotion)
 admin.site.register(PaymentCard)
+
+from django.contrib import admin
+from .models import TheatreLoggingSystem
+
+@admin.register(TheatreLoggingSystem)
+class TheatreLoggingSystemAdmin(admin.ModelAdmin):
+    list_display = ('date', 'child_purchases', 'adult_purchases', 'senior_purchases', 'total_purchases')
+    ordering = ('-date',)

86ec754ec0307ef9ef8f69bc00f2ff311c583587
indukuripavan28 <147741786+indukuripavan28@users.noreply.github.com>
2024-12-11 16:40:23 -0500

    Update admin.py

--- cinema_e_booking_system/v1/admin.py
+++ cinema_e_booking_system/v1/admin.py
@@ -6,3 +6,10 @@ admin.site.register(Booking)
 admin.site.register(Promotion)
 admin.site.register(PaymentCard)
 
+from django.contrib import admin
+from .models import TheatreLoggingSystem
+
+@admin.register(TheatreLoggingSystem)
+class TheatreLoggingSystemAdmin(admin.ModelAdmin):
+    list_display = ('date', 'child_purchases', 'adult_purchases', 'senior_purchases', 'total_purchases')
+    ordering = ('-date',)
